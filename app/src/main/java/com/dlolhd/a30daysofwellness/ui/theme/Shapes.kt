80dc57f0884764b8a6d5136d436e62539a85cb78
balay <dlolhd@gmail.com>
2023-10-20 09:24:17 +0200

    Custom Color, Shapes, Type and Theme.

---
+++ app/src/main/java/com/dlolhd/a30daysofwellness/ui/theme/Shapes.kt
@@ -0,0 +1,13 @@
+package com.dlolhd.a30daysofwellness.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small = RoundedCornerShape(8.dp),
+    medium = RoundedCornerShape(topStart = 16.dp, bottomStart = 4.dp, bottomEnd = 16.dp, topEnd = 4.dp),
+    large = RoundedCornerShape(16.dp),
+    extraLarge = RoundedCornerShape(24.dp)
+)
--- app/src/main/java/com/dlolhd/a30daysofwellness/ui/theme/Type.kt
+++ app/src/main/java/com/dlolhd/a30daysofwellness/ui/theme/Type.kt
@@ -2,33 +2,44 @@ package com.dlolhd.a30daysofwellness.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.dlolhd.a30daysofwellness.R
+
+val SrirachaRegular = FontFamily(
+    Font(R.font.sriracha_regular, FontWeight.Bold),
+    Font(R.font.sriracha_regular, FontWeight.SemiBold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
+    displayLarge = TextStyle(
+    fontFamily = SrirachaRegular,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 36.sp
+),
+    displayMedium = TextStyle(
+    fontFamily = SrirachaRegular,
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp
+),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    fontFamily = SrirachaRegular,
+    fontWeight = FontWeight.Bold,
+    fontSize = 14.sp
+),
+    bodyLarge = TextStyle(
+    fontFamily = SrirachaRegular,
+    fontWeight = FontWeight.SemiBold,
+    fontSize = 14.sp,
+    lineHeight = 20.sp,
+    letterSpacing = 0.5.sp
+),
+    displaySmall = TextStyle(
+    fontFamily = SrirachaRegular,
+    fontWeight = FontWeight.Bold,
+    fontSize = 20.sp
+)
 )
